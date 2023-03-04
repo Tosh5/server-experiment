808d82bd59f4f71b3b3777dadc1ac7d2b6a501fc
Tosh5 <toshiya.5601048.com@gmail.com>
2023-03-04 16:35:54 +0900

    22

--- index.js
+++ index.js
@@ -181,6 +181,22 @@ app.get('/', (req, res) => {
     res.send('This is home directory of 玉入れサーバ。玉入れかごロボットの移動速度（0~10の11段階）を取得するには、~/speed のエンドポイントに接続すること。')
 })
 
+// let speed = 0
+// let balance = (negIndex - posiIndex)/20 + 5 // ブーイングと応援のバランス
+// if(balance >= 10){
+//     balance = 10
+// }
+// if(balance <= 0){
+//     balance = 0
+// }
+// balance = int(balance)
+
+
+
 app.get('/speed', (req, res) => {
     res.send(negIndex/posiIndex)
+})
+
+app.get('/speed2', (req, res) => {
+    res.send(`${negIndex/posiIndex}`)
 })
